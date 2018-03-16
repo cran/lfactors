64fe9a42a8176c35575a504facdc0945dd9addc2
Paul Bailey <pbailey@air.org>
2018-03-16 20:41:08 +0000

    version 1.0.4

--- R/inlf.R
+++ R/inlf.R
@@ -1,17 +1,12 @@
-#' Implements \%in\% for lfactors
-#' @param x same as \code{\link[base]{\%in\%}}
-#' @param table same as \code{\link[base]{\%in\%}}
-#' @seealso \code{\link[base]{\%in\%}}
+# Rd in man, not roxygen
 #' @export
 inlf <- function(x, table) {
   if(inherits(x, "lfactor")) {
-  	#m1 <- match(x=as.character(x), table=as.character(table), nomatch=0)
-  	#m2 <- match(x=as.character(switchllevels(x)), table=as.character(table), nomatch=0)
-    m1 <- mlfactor(x=as.character(x), table=as.character(table), nomatch=0)
-    m2 <- mlfactor(x=as.character(switchllevels(x)), table=as.character(table), nomatch=0)
+  	m1 <- match(x=as.character(x), table=as.character(table), nomatch=0, incomparables=NULL)
+  	m2 <- match(x=as.character(switchllevels(x)), table=as.character(table), nomatch=0, incomparables=NULL)
   } else {
-  	m1 <- mlfactor(x=as.character(x), table=as.character(table), nomatch=0)
-  	m2 <- mlfactor(x=as.character(x), table=as.character(switchllevels(table)), nomatch=0)
+    m1 <- match(x=as.character(x), table=as.character(table), nomatch=0, incomparables=NULL)
+    m2 <- match(x=as.character(x), table=as.character(switchllevels(table)), nomatch=0, incomparables=NULL)
   }
   ((m1 > 0) | (m2 > 0))
 }
